03771e1281ef240292cd9ff524ce6dc2161078dc
Daryl V <daryl.villalobos@hotmail.com>
2023-06-06 11:43:33 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextBoxHorizontal
+
+def extract_text_from_pages(pdf_path, start_page, end_page):
+    for page_number, page in enumerate(extract_pages(pdf_path), start=1):
+        if start_page <= page_number <= end_page:
+            for element in page:
+                if isinstance(element, LTTextBoxHorizontal):
+                    yield element.get_text()
+
+# Specify the start and end pages
+start_page = 0
+end_page = 9
+
+# Extract text from the PDF
+text_generator = extract_text_from_pages('ir2022.pdf', start_page, end_page)
+
+# Split the text into paragraphs and write to a CSV file
+with open('file.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for text in text_generator:
+        paragraphs = text.split('\n\n')
+        for paragraph in paragraphs:
+            writer.writerow([paragraph])
